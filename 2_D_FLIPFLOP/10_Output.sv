66c6158833ad464227de107be78eda8e12b43e94
Vighnesh E S <Vighnesh1510@gmail.com>
2026-05-16 22:04:48 +0530

    Create 10_Output.sv

---
+++ 2_D_FLIPFLOP/10_Output.sv
@@ -0,0 +1,105 @@
+xcelium> source /xcelium25.03/tools/xcelium/files/xmsimrc
+xcelium> run
+time= 5 Generator , input randomized
+time= 15 Generator , input randomized
+time =15 DRIVER reset=0 d=0 Q=0
+time=20 [SCB] Checking RESET | prev_reset=1 | curr_Q=0
+time=20 [SCB] Reset case PASSED
+
+time= 25 Generator , input randomized
+time =25 DRIVER reset=0 d=0 Q=0
+time=30 [SCB] Checking D==Q | prev_d=0 | curr_Q=0
+time=30 [SCB] D==Q PASSED
+
+time= 35 Generator , input randomized
+time =35 DRIVER reset=0 d=0 Q=0
+time=40 [SCB] Checking D==Q | prev_d=0 | curr_Q=0
+time=40 [SCB] D==Q PASSED
+
+time= 45 Generator , input randomized
+time =45 DRIVER reset=1 d=1 Q=0
+time=50 [SCB] Checking D==Q | prev_d=0 | curr_Q=0
+time=50 [SCB] D==Q PASSED
+
+time= 55 Generator , input randomized
+time =55 DRIVER reset=0 d=0 Q=0
+time=60 [SCB] Checking RESET | prev_reset=1 | curr_Q=0
+time=60 [SCB] Reset case PASSED
+
+time= 65 Generator , input randomized
+time =65 DRIVER reset=0 d=0 Q=0
+time=70 [SCB] Checking D==Q | prev_d=0 | curr_Q=0
+time=70 [SCB] D==Q PASSED
+
+time= 75 Generator , input randomized
+time =75 DRIVER reset=1 d=1 Q=0
+time=80 [SCB] Checking D==Q | prev_d=0 | curr_Q=0
+time=80 [SCB] D==Q PASSED
+
+time= 85 Generator , input randomized
+time =85 DRIVER reset=0 d=0 Q=0
+time=90 [SCB] Checking RESET | prev_reset=1 | curr_Q=0
+time=90 [SCB] Reset case PASSED
+
+time= 95 Generator , input randomized
+time =95 DRIVER reset=1 d=0 Q=0
+time=100 [SCB] Checking D==Q | prev_d=0 | curr_Q=0
+time=100 [SCB] D==Q PASSED
+
+time= 105 Generator , input randomized
+time =105 DRIVER reset=0 d=0 Q=0
+time=110 [SCB] Checking RESET | prev_reset=1 | curr_Q=0
+time=110 [SCB] Reset case PASSED
+
+time= 115 Generator , input randomized
+time =115 DRIVER reset=1 d=0 Q=0
+time=120 [SCB] Checking D==Q | prev_d=0 | curr_Q=0
+time=120 [SCB] D==Q PASSED
+
+time= 125 Generator , input randomized
+time =125 DRIVER reset=0 d=1 Q=0
+time=130 [SCB] Checking RESET | prev_reset=1 | curr_Q=0
+time=130 [SCB] Reset case PASSED
+
+time= 135 Generator , input randomized
+time =135 DRIVER reset=0 d=0 Q=0
+time=140 [SCB] Checking D==Q | prev_d=1 | curr_Q=1
+time=140 [SCB] D==Q PASSED
+
+time= 145 Generator , input randomized
+time =145 DRIVER reset=1 d=0 Q=0
+time=150 [SCB] Checking D==Q | prev_d=0 | curr_Q=0
+time=150 [SCB] D==Q PASSED
+
+time= 155 Generator , input randomized
+time =155 DRIVER reset=0 d=1 Q=0
+time=160 [SCB] Checking RESET | prev_reset=1 | curr_Q=0
+time=160 [SCB] Reset case PASSED
+
+time= 165 Generator , input randomized
+time =165 DRIVER reset=1 d=1 Q=0
+time=170 [SCB] Checking D==Q | prev_d=1 | curr_Q=1
+time=170 [SCB] D==Q PASSED
+
+time= 175 Generator , input randomized
+time =175 DRIVER reset=0 d=0 Q=0
+time=180 [SCB] Checking RESET | prev_reset=1 | curr_Q=0
+time=180 [SCB] Reset case PASSED
+
+time= 185 Generator , input randomized
+time =185 DRIVER reset=1 d=1 Q=0
+time=190 [SCB] Checking D==Q | prev_d=0 | curr_Q=0
+time=190 [SCB] D==Q PASSED
+
+time= 195 Generator , input randomized
+time =195 DRIVER reset=1 d=0 Q=0
+time=200 [SCB] Checking RESET | prev_reset=1 | curr_Q=0
+time=200 [SCB] Reset case PASSED
+
+time= 205 Generator , input randomized
+time =205 DRIVER reset=0 d=0 Q=0
+time=210 [SCB] Checking RESET | prev_reset=1 | curr_Q=0
+time=210 [SCB] Reset case PASSED
+
+Simulation complete via $finish(1) at time 211 NS + 0
+./testbench.sv:20     #1 $finish;
