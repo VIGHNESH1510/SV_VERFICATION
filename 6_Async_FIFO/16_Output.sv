cd77f3ae09d112f2a258169f221579d67f78abe0
Vighnesh E S <Vighnesh1510@gmail.com>
2026-07-20 14:44:17 +0530

    Create 16_Output.sv

---
+++ 6_Async_FIFO/16_Output.sv
@@ -0,0 +1,182 @@
+xcelium> source /xcelium25.03/tools/xcelium/files/xmsimrc
+xcelium> run
+[GEN] time =20
+[R_DRI] time =20 , r_rst =0 r_en =1
+[W_DRI] time =20 | w_rst =0 | w_en =0
+[GEN] time =22
+[GEN] time =24
+[GEN] time =26
+[W_MON] time =26 | w_rst =1 | w_en =0 | d_in =2 | full =0
+[GEN] time =28
+[GEN] time =30
+[W_DRI] time =30 | w_rst =0 | w_en =0
+[R_MON] time =31 | r_rst =1 | r_en =1 | d_out =x | empty =1
+[GEN] time =32
+[GEN] time =34
+[GEN] time =36
+[W_MON] time =36 | w_rst =1 | w_en =0 | d_in =14 | full =0
+[GEN] time =38
+[GEN] time =40
+[R_DRI] time =40 , r_rst =0 r_en =1
+[W_DRI] time =40 | w_rst =0 | w_en =1
+[GEN] time =42
+[GEN] time =44
+[GEN] time =46
+[W_MON] time =46 | w_rst =1 | w_en =1 | d_in =0 | full =0
+[SB] WRITE : 0  Queue=1
+[GEN] time =48
+[GEN] time =50
+[W_DRI] time =50 | w_rst =0 | w_en =1
+[R_MON] time =51 | r_rst =1 | r_en =1 | d_out =0 | empty =1
+[SB PASS] Expected=0 Received=0
+[GEN] time =52
+[GEN] time =54
+[GEN] time =56
+[W_MON] time =56 | w_rst =1 | w_en =1 | d_in =4 | full =0
+[SB] WRITE : 4  Queue=1
+[GEN] time =58
+[GEN] time =60
+[R_DRI] time =60 , r_rst =0 r_en =0
+[W_DRI] time =60 | w_rst =0 | w_en =0
+[GEN] time =62
+[GEN] time =64
+[GEN] time =66
+[W_MON] time =66 | w_rst =1 | w_en =0 | d_in =2 | full =0
+[GEN] time =68
+[GEN] time =70
+[W_DRI] time =70 | w_rst =0 | w_en =1
+[R_MON] time =71 | r_rst =1 | r_en =0 | d_out =0 | empty =1
+[GEN] time =72
+[GEN] time =74
+[GEN] time =76
+[W_MON] time =76 | w_rst =1 | w_en =1 | d_in =9 | full =0
+[SB] WRITE : 9  Queue=2
+[GEN] time =78
+[GEN] time =80
+[R_DRI] time =80 , r_rst =0 r_en =0
+[W_DRI] time =80 | w_rst =0 | w_en =0
+[GEN] time =82
+[GEN] time =84
+[GEN] time =86
+[W_MON] time =86 | w_rst =1 | w_en =0 | d_in =10 | full =0
+[GEN] time =88
+[GEN] time =90
+[W_DRI] time =90 | w_rst =0 | w_en =1
+[R_MON] time =91 | r_rst =1 | r_en =0 | d_out =0 | empty =0
+[GEN] time =92
+[GEN] time =94
+[GEN] time =96
+[W_MON] time =96 | w_rst =1 | w_en =1 | d_in =7 | full =0
+[SB] WRITE : 7  Queue=3
+[GEN] time =98
+[GEN] time =100
+[R_DRI] time =100 , r_rst =0 r_en =1
+[W_DRI] time =100 | w_rst =0 | w_en =0
+[GEN] time =102
+[GEN] time =104
+[GEN] time =106
+[W_MON] time =106 | w_rst =1 | w_en =0 | d_in =5 | full =0
+[GEN] time =108
+[GEN] time =110
+[W_DRI] time =110 | w_rst =0 | w_en =0
+[R_MON] time =111 | r_rst =1 | r_en =1 | d_out =4 | empty =0
+[SB PASS] Expected=4 Received=4
+[GEN] time =112
+[GEN] time =114
+[GEN] time =116
+[W_MON] time =116 | w_rst =1 | w_en =0 | d_in =12 | full =0
+[GEN] time =118
+[GEN] time =120
+[R_DRI] time =120 , r_rst =0 r_en =0
+[W_DRI] time =120 | w_rst =0 | w_en =1
+[GEN] time =122
+[GEN] time =124
+[GEN] time =126
+[W_MON] time =126 | w_rst =1 | w_en =1 | d_in =7 | full =0
+[SB] WRITE : 7  Queue=3
+[GEN] time =128
+[GEN] time =130
+[W_DRI] time =130 | w_rst =0 | w_en =0
+[R_MON] time =131 | r_rst =1 | r_en =0 | d_out =4 | empty =0
+[GEN] time =132
+[GEN] time =134
+[GEN] time =136
+[W_MON] time =136 | w_rst =1 | w_en =0 | d_in =1 | full =0
+[GEN] time =138
+[GEN] time =140
+[R_DRI] time =140 , r_rst =0 r_en =1
+[W_DRI] time =140 | w_rst =0 | w_en =1
+[GEN] time =142
+[GEN] time =144
+[GEN] time =146
+[W_MON] time =146 | w_rst =1 | w_en =1 | d_in =11 | full =0
+[SB] WRITE : b  Queue=4
+[GEN] time =148
+[GEN] time =150
+[W_DRI] time =150 | w_rst =0 | w_en =1
+[R_MON] time =151 | r_rst =1 | r_en =1 | d_out =9 | empty =0
+[SB PASS] Expected=9 Received=9
+[GEN] time =152
+[GEN] time =154
+[GEN] time =156
+[W_MON] time =156 | w_rst =1 | w_en =1 | d_in =5 | full =0
+[SB] WRITE : 5  Queue=4
+[GEN] time =158
+[GEN] time =160
+[R_DRI] time =160 , r_rst =0 r_en =1
+[W_DRI] time =160 | w_rst =0 | w_en =0
+[GEN] time =162
+[GEN] time =164
+[GEN] time =166
+[W_MON] time =166 | w_rst =1 | w_en =0 | d_in =14 | full =0
+[GEN] time =168
+[GEN] time =170
+[W_DRI] time =170 | w_rst =0 | w_en =0
+[R_MON] time =171 | r_rst =1 | r_en =1 | d_out =7 | empty =0
+[SB PASS] Expected=7 Received=7
+[GEN] time =172
+[GEN] time =174
+[GEN] time =176
+[W_MON] time =176 | w_rst =1 | w_en =0 | d_in =1 | full =0
+[GEN] time =178
+[GEN] time =180
+[R_DRI] time =180 , r_rst =0 r_en =1
+[W_DRI] time =180 | w_rst =0 | w_en =1
+[GEN] time =182
+[GEN] time =184
+[GEN] time =186
+[W_MON] time =186 | w_rst =1 | w_en =1 | d_in =10 | full =0
+[SB] WRITE : a  Queue=4
+[GEN] time =188
+[GEN] time =190
+[W_DRI] time =190 | w_rst =0 | w_en =1
+[R_MON] time =191 | r_rst =1 | r_en =1 | d_out =7 | empty =0
+[SB PASS] Expected=7 Received=7
+[GEN] time =192
+[GEN] time =194
+[GEN] time =196
+[W_MON] time =196 | w_rst =1 | w_en =1 | d_in =5 | full =0
+[SB] WRITE : 5  Queue=4
+[GEN] time =198
+[GEN] time =200
+[R_DRI] time =200 , r_rst =0 r_en =1
+[W_DRI] time =200 | w_rst =0 | w_en =0
+[GEN] time =202
+[GEN] time =204
+[GEN] time =206
+[W_MON] time =206 | w_rst =1 | w_en =0 | d_in =8 | full =0
+[GEN] time =208
+[GEN] time =210
+[W_DRI] time =210 | w_rst =0 | w_en =1
+[R_MON] time =211 | r_rst =1 | r_en =1 | d_out =11 | empty =0
+[SB PASS] Expected=b Received=b
+[GEN] time =212
+[GEN] time =214
+[GEN] time =216
+[W_MON] time =216 | w_rst =1 | w_en =1 | d_in =12 | full =0
+[SB] WRITE : c  Queue=4
+[GEN] time =218
+--------------------------
+PASS = 6
+FAIL = 0
+--------------------------
